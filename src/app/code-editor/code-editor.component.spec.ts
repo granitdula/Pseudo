e19794b1e4f5ea93dfe5ec56d212b1c0d5215b64
kvrule <granitdula98@gmail.com>
2020-08-04 23:07:19 +0100

    Added content editable window

--- src/app/code-editor/code-editor.component.spec.ts
+++ src/app/code-editor/code-editor.component.spec.ts
@@ -19,7 +19,38 @@ describe('CodeEditorComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('HTML tests', () => {
+    describe('Main component test', () => {
+      it('should create', () => {
+        expect(component).toBeTruthy();
+      });
+    });
+
+    describe('Code editor tests', () => {
+      it('should create div of class code-editor', () => {
+
+        const codeEditorElement: HTMLElement = fixture.nativeElement;
+        const codeEditorClasses = codeEditorElement.getElementsByClassName('code-editor');
+
+        expect(codeEditorClasses.length).toBe(1);
+      });
+
+      it('should create div within code-editor div called input-region', () => {
+
+        const codeEditorElement: HTMLElement = fixture.nativeElement;
+        const codeEditorClasses = codeEditorElement.getElementsByClassName('code-editor');
+        const inputRegionClasses = codeEditorClasses[0].getElementsByClassName('input-region');
+
+        expect(inputRegionClasses.length).toBe(1);
+      });
+
+      it('should have input-region div which is content editable', () => {
+
+        const codeEditorElement: HTMLElement = fixture.nativeElement;
+        const inputRegionClasses = codeEditorElement.getElementsByClassName('input-region');
+
+        expect(inputRegionClasses[0].getAttribute('contenteditable')).toBe('true');
+      });
+    });
   });
 });
--- src/app/code-editor/code-editor.component.ts
+++ src/app/code-editor/code-editor.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-code-editor',
   templateUrl: './code-editor.component.html',
   styleUrls: ['./code-editor.component.css']
 })
-export class CodeEditorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class CodeEditorComponent {}
