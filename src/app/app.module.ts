8947c5ef11c3d1bc1eb503f282971c95f65f85d9
kvrule <granitdula98@gmail.com>
2020-08-15 21:11:14 +0100

    Moved navigation-bar and code-editor components to new components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { CodeEditorComponent } from './code-editor/code-editor.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { CodeEditorComponent } from './components/code-editor/code-editor.component';
 
 @NgModule({
   declarations: [
