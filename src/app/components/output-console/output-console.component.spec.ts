1057e427001cf94f8637214817a6941dbef5b62c
kvrule <granitdula98@gmail.com>
2020-09-09 19:04:09 +0100

    Added function to set text value of output console

--- src/app/components/output-console/output-console.component.spec.ts
+++ src/app/components/output-console/output-console.component.spec.ts
@@ -72,4 +72,66 @@ describe('OutputConsoleComponent', () => {
       });
     });
   });
+
+  describe('Typescript tests', () => {
+    describe('outputText tests', () => {
+      it('should have empty innerHTML when output console initialised.', () => {
+
+        const outputConsoleElement: HTMLElement = fixture.nativeElement;
+        const outputRegion = outputConsoleElement.querySelector('div.output-region');
+
+        expect(outputRegion.innerHTML).toBe('');
+      });
+
+      it('should have a line of text in innerHTML when calling outputText with that line.', () => {
+
+        const outputConsoleElement: HTMLElement = fixture.nativeElement;
+        const outputRegion = outputConsoleElement.querySelector('div.output-region');
+
+        component.outputText('Output 1.');
+
+        fixture.detectChanges();
+
+        const expected: string = 'Output 1.';
+
+        expect(outputRegion.innerHTML).toBe(expected);
+      });
+
+      it('should have 5 lines of text in innerHTML when calling outputText with 5 lines of text.', () => {
+
+        const outputConsoleElement: HTMLElement = fixture.nativeElement;
+        const outputRegion = outputConsoleElement.querySelector('div.output-region');
+
+        const htmlArr: string[] = [];
+
+        for (let i = 1; i < 6; i++) {
+          htmlArr.push(`Line ${i}`);
+        }
+
+        const htmlText: string = htmlArr.join('<br>');
+
+        component.outputText(htmlText);
+
+        fixture.detectChanges();
+
+        const expected: string = `Line 1<br>Line 2<br>Line 3<br>Line 4<br>Line 5`;
+
+        expect(outputRegion.innerHTML).toBe(expected);
+      });
+
+      it('should have an empty line of text in innerHTML when calling outputText with that empty line.', () => {
+
+        const outputConsoleElement: HTMLElement = fixture.nativeElement;
+        const outputRegion = outputConsoleElement.querySelector('div.output-region');
+
+        component.outputText('');
+
+        fixture.detectChanges();
+
+        const expected: string = '';
+
+        expect(outputRegion.innerHTML).toBe(expected);
+      });
+    });
+  });
 });
--- src/app/components/output-console/output-console.component.ts
+++ src/app/components/output-console/output-console.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-output-console',
   templateUrl: './output-console.component.html',
   styleUrls: ['./output-console.component.css']
 })
-export class OutputConsoleComponent {}
+export class OutputConsoleComponent {
+
+  // @ViewChild('outputRegion') outputRegion: ElementRef<HTMLElement>;
+  private output: string = '';
+
+  public outputText(htmlText: string): void {
+    this.output = htmlText;
+  }
+
+  public getOutput(): string {
+    return this.output;
+  }
+}
