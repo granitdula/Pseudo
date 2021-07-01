dacaf0d2a909f8ab186d69bb2fd0f1313c87e53a
granitdula <granitdula98@gmail.com>
2021-07-01 17:59:09 +0100

    Added string data types

---
+++ src/app/data-types/string-type.ts
@@ -0,0 +1,37 @@
+import { NumberType } from './number-type';
+import { RuntimeError } from './../logic/runtime-error';
+import { ValueType } from './value-type';
+
+export class StringType extends ValueType {
+
+  constructor(private value: string) {
+    super();
+  }
+
+  public addBy(other: ValueType): [ValueType, RuntimeError] {
+    if (other instanceof StringType) {
+      return [new StringType(this.value + other.getValue()).setContext(this.context), null];
+    }
+    else { return [null, this.illegalOperation()]; }
+  }
+
+  public multiplyBy(other: ValueType): [ValueType, RuntimeError] {
+    if (other instanceof NumberType) {
+      return [new StringType(this.value.repeat(other.getValue())).setContext(this.context), null];
+    }
+    else { return [null, this.illegalOperation()]; }
+  }
+
+  public isTrue(): boolean { return this.value.length > 0; }
+
+  public copy(): StringType {
+    const str = new StringType(this.value);
+
+    str.setPos(this.posStart, this.posEnd);
+    str.setContext(this.context);
+
+    return str;
+  }
+
+  public getValue(): string { return this.value; }
+}
